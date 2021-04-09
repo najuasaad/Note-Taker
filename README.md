146e2ea4febe647bc963a30ebaa8e7a0eb618602
Najua Saad <najuasaad@gmail.com>
2021-04-08 22:26:46 -0500

    first commit because we just learned heroku in class today and I didn't want to make a commit without knowing how to on there first.

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# notetaker
+# Note Taker
+
+## Description
+
+An Express.js note taking app. It uses client side requests and posts handled by server-side responses.
+
+## Usage
+
+The app begins on the homepage. After selecting "get started", the note taking can begin! Add an easy to identify title and a lengthier description. When both fields have been entered, the save button will appear. When you save it, the title will populate a tile on the left hand side. To review that note, click directly on the title. Click on the pencil to create a new note.
+
+## Installation
+
+If pulling from github repo, install express and random-id as dependencies and nodemon on dev dependencies. 
+
+## Links
+
+[Heroku](https://hidden-oasis-72157.herokuapp.com/)
+
+[Github Repo](https://github.com/najuasaad/notetaker)
+
+## Screenshots
+
+![screenshot1](pics/index.jpg)
+![screenshot2](pics/notes.jpg)
---
+++ server.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const randomId = require('random-id')
+
+const len = 4
+const pattern = 'aA0'
+let iden = randomId(len, pattern)
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+const random = Math.floor(Math.random() * 1000);
+
+//const allNotes = require('./db/db.json')
+//let allNotes = JSON.parse(fs.readFileSync("db/db.json", "utf8"))
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'))
+})
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'))
+})
+
+app.get('/assets/css/styles.css', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/assets/css/styles.css'))
+})
+
+app.get('/assets/js/index.js', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/assets/js/index.js'))
+})
+
+app.get('/api/notes', (req, res) => {
+    var data = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
+    res.json(data);
+});
+
+app.post('/api/notes', (req, res) => {
+    var allNotes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8')) 
+    var newNote = req.body;
+    newNote.id = iden
+    console.log(newNote.id)
+    allNotes.push(newNote);
+    fs.writeFileSync('./db/db.json', JSON.stringify(allNotes));
+    console.log('note saved')
+    res.json(allNotes);
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+    let id = req.params.id
+    const yallNotes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'))
+    //res.send(userId)
+    const obj = yallNotes.filter( note => note.id !== id )
+    fs.writeFileSync('./db/db.json', JSON.stringify(obj))
+    res.json(obj)
+})
+
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
